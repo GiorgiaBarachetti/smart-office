7b3a064f73c12381ec99b598c7d0ebf1177aa9d8
giorgia barachetti <giorgiabarachetti@gmail.com>
2023-06-08 09:49:10 +0200

    forgot api file in the last commit

---
+++ src/utils/fetch/api.tsx
@@ -0,0 +1,11 @@
+
+export const baseURL = process.env.REACT_APP_BASE_API_URL
+export const urlShelly = process.env.REACT_APP_BASE_SHELLY_URL
+export const urlCoffee = process.env.REACT_APP_BASE_COFFEE_URL
+export const urlAlhpa = process.env.REACT_APP_BASE_ALPHA_URL
+export const urlTplink = process.env.REACT_APP_BASE_PRINTER_URL
+
+/*
+import { baseURL, urlShelly, urlCoffee, urlAlhpa, urlTplink } from '../../utils/fetch/api';
+
+*/
