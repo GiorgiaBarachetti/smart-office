0b33ed1b5a8791a0074d67d9811fc7a74f1aae06
giorgia barachetti <giorgiabarachetti@gmail.com>
2023-06-12 10:26:00 +0200

    fix : solved problems with mui treasuy. do not install

--- src/pages/Layout/Lights/LightsPage.tsx
+++ src/pages/Layout/Lights/LightsPage.tsx
@@ -1,32 +1,31 @@
 import React from 'react';
-import { Box, Button, ButtonGroup, CardContent, CardHeader, Stack, Switch, Typography } from '@mui/material'
+import { Box, Button, ButtonGroup, CardContent, CardHeader, CardMedia, Stack, Switch, Typography } from '@mui/material'
 import TableLights from '../../../components/Tables/TableLights'
 import Card from '@mui/material/Card';
 import { Lights } from '../../../utils/interfaces/Interfaces';
 import { useState, useEffect } from 'react'
 import { baseURL, urlShelly } from '../../../utils/fetch/api';
 import { SHADOWSTYLE } from '../../../utils/const/Const';
-
-
+import Chart from '../../../components/Chart/Chart';
+import CircleIcon from '@mui/icons-material/Circle';
+import Laboratory from '../../../img/laboratory.jpg'
+//import { useCoverCardMediaStyles } from '@mui-treasury/styles/cardMedia/cover';
 
 
 const LightsPage = () => {
- // const apiUrl = process.env.LIGHTS_FETCH;
 
-  
-
- const [lightsDatasArray, setLightsDatasArray] = useState<Lights[]>([]);
- const fetchLights = async () => {
-   try {
-     const response = await fetch(`${baseURL}${urlShelly}/all/status`);
-     const data = await response?.json();
-     console.log(response, data)
-     setLightsDatasArray(data.data);
-     console.log(data);
-   } catch (error) {
-     console.log('error fetching lights', error);
-   }
- };
+  const [lightsDatasArray, setLightsDatasArray] = useState<Lights[]>([]);
+  const fetchLights = async () => {
+    try {
+      const response = await fetch(`${baseURL}${urlShelly}/all/status`);
+      const data = await response?.json();
+      console.log(response, data)
+      setLightsDatasArray(data.data);
+      console.log(data);
+    } catch (error) {
+      console.log('error fetching lights', error);
+    }
+  };
 
 
   useEffect(() => {
@@ -46,8 +45,8 @@ const LightsPage = () => {
 
   const switchOnLightById = async (key: any) => {
     try {
-      const light = lightsDatasArray.find((light) => light?.state.room === key);
-      console.log(light?.state.room);
+      const light = lightsDatasArray.find((light) => light?.room === key);
+      console.log(light?.room);
       if (light) {
         console.log(light)
         const id = light.state.id
@@ -56,11 +55,11 @@ const LightsPage = () => {
           await fetch(`${baseURL}${urlShelly}/${id}/on`, { method: 'POST' });
           //aggiorno lo stato delle luci settandolo nel setLlightsDatasArray
           setLightsDatasArray((prevState) =>
-          prevState.map((light) =>
-          //constrollo che la chiave 'room' sia uguale alla chiave key data in input e in tal caso aggiorno l'output ovvero lo stato (acceso/spento)
-            light?.state.room === key ? { ...light, state: { ...light.state, output: true } } : light
-          )
-        );
+            prevState.map((light) =>
+              //constrollo che la chiave 'room' sia uguale alla chiave key data in input e in tal caso aggiorno l'output ovvero lo stato (acceso/spento)
+              light?.room === key ? { ...light, state: { ...light.state, output: true } } : light
+            )
+          );
         } else {
         }
 
@@ -72,8 +71,8 @@ const LightsPage = () => {
   const switchOffLightById = async (key: any) => {
     console.log(key)
     try {
-      const light = lightsDatasArray.find((light) => light?.state.room === key);
-      console.log(light?.state.room);
+      const light = lightsDatasArray.find((light) => light?.room === key);
+      console.log(light?.room);
       if (light) {
         console.log(light)
         const id = light.state.id
@@ -81,10 +80,10 @@ const LightsPage = () => {
           console.log(light.state.output);
           await fetch(`${baseURL}${urlShelly}/${id}/off`, { method: 'POST' });
           setLightsDatasArray((prevState) =>
-          prevState.map((light) =>
-            light?.state.room === key ? { ...light, state: { ...light.state, output: false } } : light
-          )
-        );
+            prevState.map((light) =>
+              light?.room === key ? { ...light, state: { ...light.state, output: false } } : light
+            )
+          );
         } else {
         }
 
@@ -93,37 +92,73 @@ const LightsPage = () => {
     } catch (error) {
       console.log(`Error switching the light of the room:`, error);
     }
+    
   }
-
-  const sortedLightsDatasArray = lightsDatasArray.length ? lightsDatasArray.sort((a, b) => a.state.id - b.state.id) : [];
+  const mediaStyles = useCoverCardMediaStyles({ bgPosition: 'top' });
+  const sortedLightsDatasArray = lightsDatasArray != undefined ? lightsDatasArray.sort((a, b) => a.state.id - b.state.id) : [];
 
   return <>
-    <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} sx={{ p: '20px', borderRadius: '6px', bgcolor: 'lightgrey' , mx: 'auto', my: '30px', width: '70%', ...SHADOWSTYLE}} >
+    <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} sx={{ p: '20px', borderRadius: '6px', bgcolor: 'lightgrey', mx: 'auto', my: '30px', width: '70%', ...SHADOWSTYLE }} >
       <Typography sx={{ variant: 'h1', textAlign: 'center' }}>ROOMS</Typography>
+
       <Button onClick={() => switchAllOffLightDatas()} sx={{ width: '300px', mx: 'auto' }}>SWITCH OFF ALL THE LIGHTS</Button>
       <Box display={'flex'} flexDirection={'row'} flexWrap={'wrap'} justifyContent={'center'} sx={{ p: '19px', gap: '32px' }}>
-        {/*{lightsDatasArray?.filter((light) =>*/}
+        {/*
+        {lightsDatasArray?.filter((light) =>
+        */}
         
         {sortedLightsDatasArray?.filter((light) =>
-          light.state.room !== "----" &&
-          light.state.room !== "Punto luce non attivo"
+          light.room !== "----" &&
+          light.room !== "Punto luce non attivo"
         ).map((light) => (
-          <Card key={light.state.id} sx={{cursor:'pointer', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '201px', padding: '20px' }}>
+          <Card key={light.state.id}  sx={{ backgroundImage:`url${Laboratory}`, position:'relative', cursor: 'pointer', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '201px', padding: '20px' }}>
+            <CardMedia
+          component="img"
+          height="140"
+          image={Laboratory}
+          
+        />
+            <CircleIcon  style={{color: light.state.output ? 'green' : 'red', position:'absolute', right: '7px', top: '7px', fontSize:'20px'}} />
             {/* <CardMedia
                                 component="img"
                                 height="194"
                                 image="/static/images/cards/paella.jpg"
                                 alt="Paella dish"
                               />*/}
-            <CardContent sx={{ mx: 'auto' }}>
-              <Typography sx={{ textAlign: 'center' }}>{light.state.room}</Typography>
+            <CardContent sx={{ mx: 'auto', display:'flex', flexDirection: 'column', justifyContent:'center' }}>
+              <Typography sx={{ textAlign: 'center', pb: '10px' }}>{light.room}</Typography>
               <ButtonGroup>
-                <Button onClick={() => switchOnLightById(light.state.room)} disabled={light.state.output == true} >ON</Button>
-                <Button onClick={() => switchOffLightById(light.state.room)} disabled={light.state.output == false}>OFF</Button>
+                <Button onClick={() => switchOnLightById(light.room)} disabled={light.state.output == true} >ON</Button>
+                <Button onClick={() => switchOffLightById(light.room)} disabled={light.state.output == false}>OFF</Button>
               </ButtonGroup>
+              <Typography sx={{ textAlign: 'center', pt: '10px', fontSize:'13px' }} variant="body2">{light.state.output === true ? `Power used: ${light.state.apower}Watt`: ''}</Typography>
             </CardContent>
           </Card>
         ))}
+        <Card key={12}  sx={{ backgroundImage:`url${Laboratory}`, position:'relative', cursor: 'pointer', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '201px', padding: '20px' }}>
+            <CardMedia
+          component="img"
+          height="140"
+          image={Laboratory}
+          //classes={mediaStyles}
+          
+        />
+            <CircleIcon  style={{color: 'green' , position:'absolute', right: '7px', top: '7px', fontSize:'20px'}} />
+            {/* <CardMedia
+                                component="img"
+                                height="194"
+                                image="/static/images/cards/paella.jpg"
+                                alt="Paella dish"
+                              />*/}
+            <CardContent sx={{ mx: 'auto', display:'flex', flexDirection: 'column', justifyContent:'center' }}>
+              <Typography sx={{ textAlign: 'center', pb: '10px' }}>camera</Typography>
+              <ButtonGroup>
+                <Button >ON</Button>
+                <Button >OFF</Button>
+              </ButtonGroup>
+              <Typography sx={{ textAlign: 'center', pt: '10px', fontSize:'13px' }} variant="body2">power used: 12wat</Typography>
+            </CardContent>
+          </Card>
       </Box>
     </Box>
     <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} sx={{ padding: '10px', borderRadius: '6px', bgcolor: 'lightgrey', mx: 'auto', my: '30px', width: '70%' }} style={SHADOWSTYLE}>
@@ -131,7 +166,9 @@ const LightsPage = () => {
       <TableLights lightsDatasArray={lightsDatasArray} />
     </Box>
 
-
+    <Box component='div' display={'flex'} flexDirection={'column'} justifyContent={'center'} sx={{ padding: '10px', borderRadius: '6px', bgcolor: 'lightgrey', mx: 'auto', my: '30px', width: '70%' }} style={SHADOWSTYLE}>
+      <Chart />
+    </Box>
   </>
 }
 
