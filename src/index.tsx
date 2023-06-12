25d6263f01d3fbdefc3a17460365f88ade718da7
giorgia barachetti <giorgiabarachetti@gmail.com>
2023-06-12 12:57:47 +0200

    fix: changing reactdomroom because this version isnt available on react v.17

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
 
 const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement!);
 
-root.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  rootElement
 );
