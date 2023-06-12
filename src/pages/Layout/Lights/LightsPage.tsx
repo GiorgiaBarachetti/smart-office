2a3da907fa2e226f52fa235f4fda9331f6faa7e9
giorgia barachetti <giorgiabarachetti@gmail.com>
2023-06-12 11:23:35 +0200

    feat: adding images to cards, missing images: flavios room and openspace

--- src/pages/Layout/Lights/LightsPage.tsx
+++ src/pages/Layout/Lights/LightsPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button, ButtonGroup, CardContent, CardHeader, CardMedia, Stack, Switch, Typography } from '@mui/material'
+import { Box, Button, ButtonGroup, CardActionArea, CardContent, CardHeader, CardMedia, Stack, Switch, Typography } from '@mui/material'
 import TableLights from '../../../components/Tables/TableLights'
 import Card from '@mui/material/Card';
 import { Lights } from '../../../utils/interfaces/Interfaces';
@@ -9,26 +9,69 @@ import { SHADOWSTYLE } from '../../../utils/const/Const';
 import Chart from '../../../components/Chart/Chart';
 import CircleIcon from '@mui/icons-material/Circle';
 import Laboratory from '../../../img/laboratory.jpg'
-//import { useCoverCardMediaStyles } from '@mui-treasury/styles/cardMedia/cover';
+import Andrea from '../../../img/andrea.jpg'
+//import Flavio from '../../../img/flavio.jpg'
+import Meeting from '../../../img/meeting.jpg'
+import Kitchen from '../../../img/kitchen.jpg'
+import Entrance from '../../../img/entrance.jpg'
+import Breaktime from '../../../img/breaktime.jpg'
+import OpenSpace from './Rooms/OpenSpace';
+//import Openspace from '../../../img/openspace.jpg'
 
+const ROOMPHOTOS = [
+  {
+    id: 0,
+    src: Andrea
+  },
+  {
+    id: 1,
+    src: Meeting
+  },
+  /*
+  {
+    id: 2,
+    src: Flavio
+  },
+  */
+  {
+    id: 3,
+    src: Laboratory
+  },
+  {
+    id: 4,
+    src: Kitchen
+  },
+  {
+    id: 6,
+    src: Breaktime
+  },
+  {
+    id: 5,
+    src: Entrance
+  },
+  /*
+  {
+    id: 7,
+    src: OpenSpace
+  },
+  */
+
+]
 
 const LightsPage = () => {
- // const apiUrl = process.env.LIGHTS_FETCH;
 
-  
-
- const [lightsDatasArray, setLightsDatasArray] = useState<Lights[]>([]);
- const fetchLights = async () => {
-   try {
-     const response = await fetch(`${baseURL}${urlShelly}/all/status`);
-     const data = await response?.json();
-     console.log(response, data)
-     setLightsDatasArray(data.data);
-     console.log(data);
-   } catch (error) {
-     console.log('error fetching lights', error);
-   }
- };
+  const [lightsDatasArray, setLightsDatasArray] = useState<Lights[]>([]);
+  const fetchLights = async () => {
+    try {
+      const response = await fetch(`${baseURL}${urlShelly}/all/status`);
+      const data = await response?.json();
+      console.log(response, data)
+      setLightsDatasArray(data.data);
+      console.log(data);
+    } catch (error) {
+      console.log('error fetching lights', error);
+    }
+  };
 
 
   useEffect(() => {
@@ -95,10 +138,20 @@ const LightsPage = () => {
     } catch (error) {
       console.log(`Error switching the light of the room:`, error);
     }
-    
+
   }
 
   const sortedLightsDatasArray = lightsDatasArray.length ? lightsDatasArray.sort((a, b) => a.state.id - b.state.id) : [];
+
+  const getRoomPhotoById = (id: number) => {
+    const roomPhoto = ROOMPHOTOS.find((photo) => photo.id === id);
+    if (roomPhoto) {
+      return roomPhoto.src;
+    }
+    // Return a default image source if the id doesn't match any room photo
+    return undefined;
+  };
+
 
   return <>
     <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} sx={{ p: '20px', borderRadius: '6px', bgcolor: 'lightgrey', mx: 'auto', my: '30px', width: '70%', ...SHADOWSTYLE }} >
@@ -106,60 +159,33 @@ const LightsPage = () => {
 
       <Button onClick={() => switchAllOffLightDatas()} sx={{ width: '300px', mx: 'auto' }}>SWITCH OFF ALL THE LIGHTS</Button>
       <Box display={'flex'} flexDirection={'row'} flexWrap={'wrap'} justifyContent={'center'} sx={{ p: '19px', gap: '32px' }}>
-        {/*{lightsDatasArray?.filter((light) =>*/}
-        
+
         {sortedLightsDatasArray?.filter((light) =>
           light.room !== "----" &&
           light.room !== "Punto luce non attivo"
         ).map((light) => (
-          <Card key={light.state.id}  sx={{ backgroundImage:`url${Laboratory}`, position:'relative', cursor: 'pointer', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '201px', padding: '20px' }}>
-            <CardMedia
-          component="img"
-          height="140"
-          image={Laboratory}
-          
-        />
-            <CircleIcon  style={{color: light.state.output ? 'green' : 'red', position:'absolute', right: '7px', top: '7px', fontSize:'20px'}} />
-            {/* <CardMedia
-                                component="img"
-                                height="194"
-                                image="/static/images/cards/paella.jpg"
-                                alt="Paella dish"
-                              />*/}
-            <CardContent sx={{ mx: 'auto', display:'flex', flexDirection: 'column', justifyContent:'center' }}>
-              <Typography sx={{ textAlign: 'center', pb: '10px' }}>{light.room}</Typography>
-              <ButtonGroup>
-                <Button onClick={() => switchOnLightById(light.room)} disabled={light.state.output == true} >ON</Button>
-                <Button onClick={() => switchOffLightById(light.room)} disabled={light.state.output == false}>OFF</Button>
-              </ButtonGroup>
-              <Typography sx={{ textAlign: 'center', pt: '10px', fontSize:'13px' }} variant="body2">{light.state.output === true ? `Power used: ${light.state.apower}Watt`: ''}</Typography>
-            </CardContent>
+          <Card key={light.state.id} sx={{ display: 'flex', flexDirection: 'column', width: '201px' }}>
+            <CardActionArea>
+              {getRoomPhotoById(light.state.id) ? (
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={getRoomPhotoById(light.state.id)}
+                  sx={{ padding: '0' }}
+                />
+              ) : null}
+              <CircleIcon style={{ color: light.state.output ? 'green' : 'red', position: 'absolute', right: '7px', top: '7px', fontSize: '20px' }} />
+              <CardContent sx={{ p: '20px', mx: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                <Typography sx={{ textAlign: 'center', pb: '10px' }}>{light.room}</Typography>
+                <ButtonGroup sx={{ alignSelf: 'center' }}>
+                  <Button onClick={() => switchOnLightById(light.room)} disabled={light.state.output == true} >ON</Button>
+                  <Button onClick={() => switchOffLightById(light.room)} disabled={light.state.output == false}>OFF</Button>
+                </ButtonGroup>
+                <Typography sx={{ textAlign: 'center', pt: '10px', fontSize: '13px' }} variant="body2">{light.state.output === true ? `Power used: ${light.state.apower}Watt` : ''}</Typography>
+              </CardContent>
+            </CardActionArea>
           </Card>
         ))}
-        <Card key={12}  sx={{ backgroundImage:`url${Laboratory}`, position:'relative', cursor: 'pointer', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '201px', padding: '20px' }}>
-            <CardMedia
-          component="img"
-          height="140"
-          image={Laboratory}
-          //classes={mediaStyles}
-          
-        />
-            <CircleIcon  style={{color: 'green' , position:'absolute', right: '7px', top: '7px', fontSize:'20px'}} />
-            {/* <CardMedia
-                                component="img"
-                                height="194"
-                                image="/static/images/cards/paella.jpg"
-                                alt="Paella dish"
-                              />*/}
-            <CardContent sx={{ mx: 'auto', display:'flex', flexDirection: 'column', justifyContent:'center' }}>
-              <Typography sx={{ textAlign: 'center', pb: '10px' }}>camera</Typography>
-              <ButtonGroup>
-                <Button >ON</Button>
-                <Button >OFF</Button>
-              </ButtonGroup>
-              <Typography sx={{ textAlign: 'center', pt: '10px', fontSize:'13px' }} variant="body2">power used: 12wat</Typography>
-            </CardContent>
-          </Card>
       </Box>
     </Box>
     <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} sx={{ padding: '10px', borderRadius: '6px', bgcolor: 'lightgrey', mx: 'auto', my: '30px', width: '70%' }} style={SHADOWSTYLE}>
