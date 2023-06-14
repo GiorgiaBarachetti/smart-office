1dfdcc95f349379a67a060b537daeec7fdb77f75
giorgia barachetti <giorgiabarachetti@gmail.com>
2023-06-14 17:53:33 +0200

    feat: adding chart js library, adding chart on rooms

--- src/components/Chart/ChartPage.tsx
+++ src/components/Chart/ChartPage.tsx
@@ -1,124 +1,128 @@
-import React, { ChangeEvent, useEffect, useState } from 'react';
+import { Chart } from "react-google-charts";
+import React, { useEffect, useState } from 'react';
 import Paper from '@mui/material/Paper';
-import { Box, Button, LinearProgress, Typography } from '@mui/material'
-import { AreaSeries, EventTracker } from '@devexpress/dx-react-chart';
-import {
-  ArgumentAxis,
-  ValueAxis,
-  Chart,
-  Tooltip,
-  ZoomAndPan,
-} from '@devexpress/dx-react-chart-material-ui';
-import { ValueScale} from '@devexpress/dx-react-chart';
-//import { Tooltip } from 'devextreme-react';
+import { Box, Button, LinearProgress, Typography } from '@mui/material';
+import { SHADOWSTYLE } from "../../utils/const/Const";
 
-interface Props {
-  id?: number;
-  watt?: number | undefined;
-}
-
-interface ChartData {
-  time: string;
-  watt: number;
-}
-
-const ChartPage = ({ id }: Props) => {
-  //const [firstDate, setFirstDate] = useState(new Date());
-  //const [secondDate, setSecondDate] = useState(new Date());
-  const [lightsDatasArray, setLightsDatasArray] = useState<ChartData[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-/*
-  const handleDateChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const selectedDate = new Date(event.target.value);
-    if (!isNaN(selectedDate.getTime())) {
-      setFirstDate(selectedDate);
+const SecondChart = () => {
+    interface ChartData {
+        time: string;
+        watt: number;
     }
-  };
 
-  const handleDateChangeSecond = (event: ChangeEvent<HTMLInputElement>) => {
-    const selectedDate = new Date(event.target.value);
-    if (!isNaN(selectedDate.getTime())) {
-      setSecondDate(selectedDate);
+    const [lightsDatasArray, setLightsDatasArray] = useState<ChartData[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    let [selectedDateRange, setSelectedDateRange] = useState('today');
+
+    const handleDateRangeClick = (dateRange: string) => {
+        setSelectedDateRange(dateRange);
+        fetchLightsData(dateRange)
     }
-  };*/
 
-  const fetchLightsData = async () => {
-    try {
-      setIsLoading(true);
-    //const response = await fetch(`http://192.168.1.6:3000/api/shelly/0/data?start=${firstDate.toISOString()}&end=${secondDate.toISOString()}`);
-      const response = await fetch(`http://192.168.1.6:3000/api/shelly/0/data?start=2023/06/08&end=2023/06/09`);
-      const data = await response.json();
-      console.log(response, data);
-      setLightsDatasArray(Array.isArray(data) ? data : [data]);
-      setIsLoading(false);
-    } catch (error) {
-      console.log('Error fetching lights data', error);
-    }
-  };
-/*
-  const resetDatas = () => {
-    const currentDate = new Date();
-    const today = currentDate.toISOString().substr(0, 10);
-    const todayDate = new Date(today);
-    if (!isNaN(todayDate.getTime())) {
-      setSecondDate(todayDate);
-    }
-    const currentDate2 = new Date();
-    const today2 = currentDate2.toISOString().substr(0, 10);
-    const todayDate2 = new Date(today2);
-    if (!isNaN(todayDate2.getTime())) {
-      setSecondDate(todayDate2);
-    }
-  };
+    const fetchLightsData = async (range:string) => {
+        try {
+            setIsLoading(true);
+            const currentDate = new Date();
+            let startDate = '';
+            let endDate = currentDate;
 
-  const [eror, seterror] = useState('')
-  const startFetch = () => {
-    if(!lightsDatasArray.length){
-      fetchLightsData()
-    }else{
-      resetDatas()
-      seterror('no dats')
-    }
-  }
-  <label>Select a start date:</label>
-  <input type="date" value={firstDate.toISOString().substr(0, 10)} onChange={handleDateChange} />
-  <label>Select an end date:</label>
-  <input type="date" value={secondDate.toISOString().substr(0, 10)} onChange={handleDateChangeSecond} />
-  <Button onClick={()=>startFetch()}>SEE CHART</Button>
-  <Typography>{eror}</Typography>
-  */
+            switch (range) {
+                case 'today':
+                    startDate = currentDate.toISOString().split('T')[0];
+                    break;
+                case 'yesterday':
+                    const yesterday = new Date(currentDate.getTime() - 24 * 60 * 60 * 1000);
+                    startDate = yesterday.toISOString().split('T')[0];
+                    break;
+                case 'lastWeek':
+                    const lastWeekStart = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+                    startDate = lastWeekStart.toISOString().split('T')[0];
+                    break;
+                case 'lastMonth':
+                    const lastMonthStart = new Date(currentDate.getTime() - 30 * 24 * 60 * 60 * 1000);
+                    startDate = lastMonthStart.toISOString().split('T')[0];
+                    break;
+                default:
+                    break;
+            }
+            const response = await fetch(`http://192.168.1.6:3000/api/shelly/0/data?start=${startDate}T00:00:00&end=${endDate}`);
+            const data = await response.json();
+            setLightsDatasArray(Array.isArray(data) ? data : [data]);
+            setIsLoading(false);
+        } catch (error) {
+            console.log('Error fetching lights data', error);
+        }
+    };
 
-  useEffect(() => {
-    fetchLightsData();
-  }, []);
+    useEffect(() => {
+        fetchLightsData('today');
+    }, []);
+
+    const options = {
+        hAxis: { title: "Time", titleTextStyle: { color: "#333" }}, gridlines: {
+            count: -1,
+            units: {
+              days: {format: ['dd/MM/YY']},
+              hours: {format: ['HH']},
+              minutes: {format: ['HH:mm']},
+            }
+          },
+          minorGridlines: {
+            units: {
+              days: {format: ['dd/MM/YY']},
+              hours: {format: ['HH']},
+              minutes: {format: ['HH:mm']},
+            }
+          },
+        vAxis: {title: "Watt", minValue: 0 },
+        //backgroundColor:'rgba(255, 255, 255, 0.8)',
+        chartArea: { width: "50%", height: "60%" },
+    };
 
 
-  
 
- return (
-   <Box sx={{ padding: '20px' }}>
-      <Paper>
-        {isLoading ? (
-          <LinearProgress/>
-        ) : (
-          <Chart data={lightsDatasArray}>
-            <ValueScale name="watt" />
-            <ArgumentAxis />
-            <ValueAxis scaleName="watt" showGrid={false} showLine={true} showTicks={true} />
-            <AreaSeries
-              name="Total Transactions"
-              valueField="watt"
-              argumentField="time"
-              scaleName="watt"
-            />
-            <EventTracker />
-            <Tooltip/>
-            <ZoomAndPan />
-          </Chart>
-        )}
-      </Paper>
-    </Box>
-  );
+    return (
+        <Box sx={{ padding: '20px', ...SHADOWSTYLE }}>
+            <Paper>
+                <Box sx={{ display: 'flex',flexDirection:'column', justifyContent: 'center', pt: '20px' }}>
+                    <Typography variant='h6' textAlign={'center'}>Select a range of data</Typography>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
+                        <Button sx={{ mx: '10px', ...SHADOWSTYLE }} variant={selectedDateRange === 'today' ? 'contained' : 'outlined'} onClick={() => handleDateRangeClick('today')}>
+                            Today
+                        </Button>
+                        <Button sx={{ mx: '10px', ...SHADOWSTYLE }} variant={selectedDateRange === 'yesterday' ? 'contained' : 'outlined'} onClick={() => handleDateRangeClick('yesterday')}>
+                            LAST 48 HOURS
+                        </Button>
+                        <Button sx={{ mx: '10px', ...SHADOWSTYLE }} variant={selectedDateRange === 'lastWeek' ? 'contained' : 'outlined'} onClick={() => handleDateRangeClick('lastWeek')}>
+                            LAST WEEK
+                        </Button>
+                        <Button sx={{ mx: '10px', ...SHADOWSTYLE }} variant={selectedDateRange === 'lastMonth' ? 'contained' : 'outlined'} onClick={() => handleDateRangeClick('lastMonth')}>
+                            LAST MONTH
+                        </Button>
+                    </Box>
+                </Box>
+                {isLoading ? (
+                    <LinearProgress />
+                ) : (
+                    <Chart
+                        border-radius={'5px'}
+                        width={'100%'}
+                        height={450}
+                        chartType="LineChart"
+                        data={[
+                            ['time', 'watt'],
+                            ...lightsDatasArray.map(({ time, watt }) => {
+                                const date = new Date(time);
+                                return  [date, watt];
+                            }),
+                        ]}
+                        options={options}
+                    />
+                )}
+
+            </Paper>
+        </Box>
+    );
 };
 
-export default ChartPage;
+export default SecondChart;
--- src/pages/Layout/Lights/Rooms/AndreaOffice.tsx
+++ src/pages/Layout/Lights/Rooms/AndreaOffice.tsx
@@ -4,6 +4,7 @@ import TableRooms from '../../../../components/Tables/TableRooms';
 import background from '../../../../img/stanzePages/andrea.jpg'
 import { SHADOWSTYLE } from '../../../../utils/const/Const';
 import ChartPage2 from '../../../../components/Chart/ChartPage';
+import SecondChart from '../../../../components/Chart/ChartPage';
 
 const AndreaOffice = () => {
   const id = 0;
@@ -13,7 +14,10 @@ const AndreaOffice = () => {
       <Box paddingTop={'30px'} paddingBottom={'10px'}>
         <Box component='div' display={'flex'} flexDirection={'column'} justifyContent={'center'} sx={{ padding: '10px', borderRadius: '6px', bgcolor: 'rgba(211, 211, 211,0.4)', mx: 'auto', my: '30px', width: '90%', heigth: '40%' }} style={SHADOWSTYLE}>
           <TableRooms idRoom={id} />
+          {/*
           <ChartPage2 />
+          */}
+          <SecondChart/>
         </Box>
       </Box>
     </div>
