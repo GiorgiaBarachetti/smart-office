61980bfa3d3e1e8e35a0a82fc5e5fe20ff3af052
giorgia barachetti <giorgiabarachetti@gmail.com>
2023-06-20 17:32:42 +0200

    feat: adding auto transition gradient background

--- src/pages/Layout/Niveus/Niveus.tsx
+++ src/pages/Layout/Niveus/Niveus.tsx
@@ -7,6 +7,7 @@ import { SHADOWSTYLE } from '../../../utils/const/Const';
 import background from '../../../img/niveus.png'
 import TableNiveus from '../../../components/Tables/TableNiveus';
 import ChartNiveus from '../../../components/Chart/ChartNiveus';
+import './style.css'
 
 const NiveusPage = () => {
   const [isLoading, setIsLoading] = useState(false)
@@ -31,7 +32,7 @@ const NiveusPage = () => {
   return (
     <div style={{ backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', marginTop: '-27px' }} >
       <Box component='div' paddingTop={'30px'} paddingBottom={'430px'}>
-        <Box component='div' display={'flex'} flexDirection={'column'} justifyContent={'center'} sx={{ padding: '10px', borderRadius: '6px', bgcolor: '#d3d3d382', mx: 'auto', my: '30px', width: { xs: '80%', sm: '80%', md: '70%' }, ...SHADOWSTYLE }} >
+        <Box component='div' className="test" display={'flex'} flexDirection={'column'} justifyContent={'center'} sx={{ padding: '10px', borderRadius: '6px', bgcolor: '#d3d3d382', mx: 'auto', my: '30px', width: { xs: '80%', sm: '80%', md: '70%' }, ...SHADOWSTYLE }} >
           <Typography variant='h6' sx={{color: 'white', mt: '10px', variant: 'h1', textAlign: 'center' }}>CONSUMES</Typography>
           <Box component='div' sx={{ padding: '1px', borderRadius: '11px', mx: 'auto', my: '30px', width: { xs: '80%', sm: '80%', md: '70%' }, ...SHADOWSTYLE }} >
             <TableNiveus niveus={niveusData}  loading={isLoading} />
