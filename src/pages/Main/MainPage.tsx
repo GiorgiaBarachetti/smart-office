a2fc27de94c6454dc0b8a561b6a7e20b2140b066
giorgia barachetti <giorgiabarachetti@gmail.com>
2023-06-22 15:16:40 +0200

    fix: deleting commented code

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -116,7 +116,6 @@ const MainPage = () => {
 
   const fetchLights = async () => {
     try {
-      //setIsLoading(true);
       const response = await fetch(`${baseURL}${urlShelly}/all/status`);
       const data = await response?.json();
       setLightsDatasArray(data.data);
@@ -130,12 +129,10 @@ const MainPage = () => {
   const [coffeeDatas, setCoffeeDatas] = useState<Coffee[]>([]);
   const fetchCoffee = async () => {
     try {
-      //setIsLoading(true);
       const response = await fetch(`${baseURL}${urlCoffee}/data`);
       const data = await response?.json();
       //Array.isArray(data) ? data : [data] senno dice che coffeeDatas non è una function
       setCoffeeDatas(Array.isArray(data) ? data : [data]);
-      //setIsLoading(false);
     } catch (error) {
       console.log('Error fetching coffee:', error);
     }
@@ -144,12 +141,10 @@ const MainPage = () => {
   const [energyDatas, setEnergyDatas] = useState<Energy[]>([]);
   const fetchEnergy = async () => {
     try {
-      //setIsLoading(true);
       const response = await fetch(`${baseURL}${urlAlhpa}/registers`);
       const data = await response?.json();
       //Array.isArray(data) ? data : [data] senno dice che coffeeDatas non è una function
       setEnergyDatas(Array.isArray(data) ? data : [data]);
-      //setIsLoading(false);
     } catch (error) {
       console.log('Error fetching coffee:', error);
     }
@@ -159,12 +154,10 @@ const MainPage = () => {
 
   const fetchNiveus = async () => {
     try {
-      //setIsLoading(true)
       const response = await fetch(`${baseURL}${urlNiveus}/registers`);
       const data = await response?.json();
       setNiveusData(Array.isArray(data) ? data : [data]);
       console.log(data)
-      //setIsLoading(false)
     } catch (error) {
       console.log('not found datas of niveus');
     }
@@ -174,12 +167,10 @@ const MainPage = () => {
 
   const fetchPrinter = async () => {
     try {
-      //setIsLoading(true);
       const response = await fetch(`${baseURL}${urlTplink}/data`);
       const data = await response?.json();
       //Array.isArray(data) ? data : [data] senno dice che printerDatas non è una function
       setPrinterDatas(Array.isArray(data) ? data : [data]);
-      //setIsLoading(false);
     } catch (error) {
       console.log('Error fetching coffee:', error);
     }
@@ -205,28 +196,28 @@ const MainPage = () => {
     const intervalNiveus = setTimeout(() => fetchNiveus(), 1000)
     const timeoutPrinter = setTimeout(() => fetchPrinter(), 1000)
     const timeoutPrinterStatus = setTimeout(() => fetchPrinterStatus(), 1000)
-   
-   return () => {
+
+    return () => {
       clearTimeout(intervalCoffee)
       clearTimeout(intervalEnergy)
       clearTimeout(intervalNiveus)
       clearTimeout(timeoutPrinter)
       clearTimeout(timeoutPrinterStatus)
-   
+
     }
   }, []);
 
   useEffect(() => {
     setIsLoading(true)
 
-    const timeoutLights = setTimeout(() => fetchLights(), 10000)
+    const timeoutLights = setTimeout(() => fetchLights(), 1000)
     return () => {
       clearTimeout(timeoutLights)
     }
 
   }, []);
 
-//useeffect per fare interval 
+  //useeffect per fare interval 
   useEffect(() => {
     const intervalCoffee = setInterval(() => fetchCoffee(), 10000)
     const intervalEnergy = setInterval(() => fetchEnergy(), 10000)
@@ -234,16 +225,15 @@ const MainPage = () => {
     const intervalPrinter = setInterval(() => fetchPrinter(), 10000)
     const intervalPrinterStatus = setInterval(() => fetchPrinterStatus(), 10000)
     const intervalLights = setTimeout(() => fetchLights(), 10000)
-   
-   return () => {
-      //setIsLoading(true)
+
+    return () => {
       clearInterval(intervalCoffee)
       clearInterval(intervalEnergy)
       clearInterval(intervalNiveus)
       clearInterval(intervalPrinter)
       clearInterval(intervalPrinterStatus)
       clearInterval(intervalLights)
-    
+
     }
   }, []);
 
@@ -295,7 +285,8 @@ const MainPage = () => {
     let y = 0;
 
     switch (roomId) {
-      case 0: // UFFICIO ANDREA
+      // UFFICIO ANDREA
+      case 0:
         x = 1000;
         y = -70;
         break;
@@ -372,109 +363,102 @@ const MainPage = () => {
     return { x, y };
   };
 
-  
+
   return (
     <div>
-      {/*MOSTRA Il loader se è true  */}
-      {isLoading && (<CircularProgress sx={{position: 'absolute', top:10, right:10}}/>)}
-
+      {isLoading && (<CircularProgress sx={{ position: 'absolute', top: 10, right: 10 }} />)}
       <svg viewBox="0 0 1280 720" preserveAspectRatio="xMinYMin meet" >
-        <image href={planimetry} width={'100%'} style={{ position: 'relative' }}/>
+        <image href={planimetry} width={'100%'} style={{ position: 'relative' }} />
         {lightsDatasArray && lightsDatasArray.length > 0 ? (
-            lightsDatasArray
-              .filter((light) => light.state && light.state.id !== 8 && light.state.id !== 9)
-              .map((light) => {
-                const { x, y } = getCoordinates(light.state.id);
-                return (
-                  <g key={light.state.id} style={{ cursor: 'pointer' }}>
-                    <SvgIcon component={LightbulbIcon} x={x} y={y} width="80px" style={lightStyle} onClick={() => openModalLights(light.state.id)} />
-                    <rect x={x + 50} y={y + 300} width="125px" height="50px" fill="#ffef3c66" rx="5px" ry="5px" />
-                    <text x={x + 60} y={y + 320} fill="black" fontSize="15px">
-                      <tspan>{`Power: ${light.state.apower} W`}</tspan>
-                    </text>
-                    <text x={x + 60} y={y + 340} fill="black" fontSize="15px">
-                      <tspan>{`Output: ${light.state.output === true ? 'ON' : 'OFF'}`}</tspan>
-                    </text>
-                  </g>
-                );
-              })
-          ) : (
-            'lightsDatasArray is empty'
-          )}
+          lightsDatasArray
+            .filter((light) => light.state && light.state.id !== 8 && light.state.id !== 9)
+            .map((light) => {
+              const { x, y } = getCoordinates(light.state.id);
+              return (
+                <g key={light.state.id} style={{ cursor: 'pointer' }}>
+                  <SvgIcon component={LightbulbIcon} x={x} y={y} width="80px" style={lightStyle} onClick={() => openModalLights(light.state.id)} />
+                  <rect x={x + 50} y={y + 300} width="125px" height="50px" fill="#ffef3c66" rx="5px" ry="5px" />
+                  <text x={x + 60} y={y + 320} fill="black" fontSize="15px">
+                    <tspan>{`Power: ${light.state.apower} W`}</tspan>
+                  </text>
+                  <text x={x + 60} y={y + 340} fill="black" fontSize="15px">
+                    <tspan>{`Output: ${light.state.output === true ? 'ON' : 'OFF'}`}</tspan>
+                  </text>
+                </g>
+              );
+            })
+        ) : (
+          'lightsDatasArray is empty'
+        )}
 
         {/*cordinate rect: x = 300+50 e y = 60 + 300*/}
 
         {coffeeDatas.length ? (
-            coffeeDatas.map((coffee) => {
-              const { x, y } = getCoordinates(coffee.coffes.id);
-              return (
-                <g key={coffee.coffes.id} style={{ ...coffeeStyle, cursor: 'pointer' }} >
-                  <SvgIcon component={CoffeeMakerIcon} x={x} y={y} width="80px" onClick={() => openCoffeeModal(coffee.coffes.id)} />
-                  <rect x={x + 50} y={y + 300} width="140px" height="50px" fill="rgba(167,156,156,0.53)" rx="5px" ry="5px" />
-                  <text x={x + 70} y={y + 330} fontSize="15px">
-                    <tspan>{`Power: ${coffee.data.macchinettaCaffe?.receivedData?.watt !== undefined ? coffee.data.macchinettaCaffe?.receivedData?.watt : '0'} W`}</tspan>
-                  </text>
-                  {/* 
-            <text x={350} y={360} fill="black" fontSize="15px">
-            <tspan>{`Output: ${light.state.output === true ? 'ON' : 'OFF'}`}</tspan>
-            </text>
-                   */}
-                </g>
-              )
-            })
-          ) : (
-            'CoffeeDatas array is empty'
-          )}
+          coffeeDatas.map((coffee) => {
+            const { x, y } = getCoordinates(coffee.coffes.id);
+            return (
+              <g key={coffee.coffes.id} style={{ ...coffeeStyle, cursor: 'pointer' }} >
+                <SvgIcon component={CoffeeMakerIcon} x={x} y={y} width="80px" onClick={() => openCoffeeModal(coffee.coffes.id)} />
+                <rect x={x + 50} y={y + 300} width="140px" height="50px" fill="rgba(167,156,156,0.53)" rx="5px" ry="5px" />
+                <text x={x + 70} y={y + 330} fontSize="15px">
+                  <tspan>{`Power: ${coffee.data.macchinettaCaffe?.receivedData?.watt !== undefined ? coffee.data.macchinettaCaffe?.receivedData?.watt : '0'} W`}</tspan>
+                </text>
+              </g>
+            )
+          })
+        ) : (
+          'CoffeeDatas array is empty'
+        )}
 
         {niveusData.length ? (
-            niveusData.map((niveus) => {
-              const { x, y } = getCoordinates(niveus.id);
-              return (
-                <g key={niveus.id} style={{ ...niveusStyle, cursor: 'pointer' }}>
-                  <SvgIcon component={AirIcon} x={x} y={y} width="80px" onClick={() => openNiveusModal(niveus.id)} />
-                  <rect x={x + 50} y={y + 300} width="125px" height="40px" fill="rgba(167,156,156,0.53)" rx="5px" ry="5px" />
-                  <text x={x + 60} y={y + 325} fill="black" fontSize="15px">
-                    <tspan>{`Power: ${niveus.data.receivedData.watt !== undefined ? niveus.data.receivedData.watt : ''} W`}</tspan>
-                  </text>
-                </g>
-              )
-            })
-          ) : ('niveusData is empty')}
+          niveusData.map((niveus) => {
+            const { x, y } = getCoordinates(niveus.id);
+            return (
+              <g key={niveus.id} style={{ ...niveusStyle, cursor: 'pointer' }}>
+                <SvgIcon component={AirIcon} x={x} y={y} width="80px" onClick={() => openNiveusModal(niveus.id)} />
+                <rect x={x + 50} y={y + 300} width="125px" height="40px" fill="rgba(167,156,156,0.53)" rx="5px" ry="5px" />
+                <text x={x + 60} y={y + 325} fill="black" fontSize="15px">
+                  <tspan>{`Power: ${niveus.data.receivedData.watt !== undefined ? niveus.data.receivedData.watt : ''} W`}</tspan>
+                </text>
+              </g>
+            )
+          })
+        ) : ('niveusData is empty')}
 
         {energyDatas.length ? (
-            energyDatas.map((energy) => {
-              const { x, y } = getCoordinates(energy.id);
-              return (
-                <g key={energy.id} style={{ ...boltStyle, cursor: 'pointer' }}>
-                  <SvgIcon component={BoltIcon} x={x} y={y} width="80px" onClick={() => openEnergyModal(energy.id)} />
-                  <rect x={x + 50} y={y + 300} width="125px" height="40px" fill="rgba(167,156,156,0.53)" rx="5px" ry="5px" />
-                  <text x={x + 60} y={y + 325} fill="black" fontSize="15px">
-                    <tspan>{`Power: ${energy.powerUsed !== undefined ? energy.powerUsed : ''} W`}</tspan>
-                  </text>
-                </g>
-              )
-            })
-          ) : (
-            'energyDatas is empty'
-          )
+          energyDatas.map((energy) => {
+            const { x, y } = getCoordinates(energy.id);
+            return (
+              <g key={energy.id} style={{ ...boltStyle, cursor: 'pointer' }}>
+                <SvgIcon component={BoltIcon} x={x} y={y} width="80px" onClick={() => openEnergyModal(energy.id)} />
+                <rect x={x + 50} y={y + 300} width="125px" height="40px" fill="rgba(167,156,156,0.53)" rx="5px" ry="5px" />
+                <text x={x + 60} y={y + 325} fill="black" fontSize="15px">
+                  <tspan>{`Power: ${energy.powerUsed !== undefined ? energy.powerUsed : ''} W`}</tspan>
+                </text>
+              </g>
+            )
+          })
+        ) : (
+          'energyDatas is empty'
+        )
         }
 
         {printerDatas.length ? (
-            printerDatas.map((printer) => {
-              const { x, y } = getCoordinates(printer.tplinkStampante.id);
-              return (
-                <g key={printer.tplinkStampante.id} style={{ ...printerStyle, cursor: 'pointer' }}>
-                  <SvgIcon component={PrintIcon} x={x} y={y} width="80px" onClick={() => openPrinterModal(printer.tplinkStampante.id)} />
-                  <rect x={x + 50} y={y + 300} width="140px" height="40px" fill="rgba(167,156,156,0.53)" rx="5px" ry="5px" />
-                  <text x={x + 60} y={y + 325} fill="black" fontSize="15px">
-                    <tspan>{`Power: ${printer.tplinkStampante.power.value !== undefined ? printer.tplinkStampante.power.value : ''} W`}</tspan>
-                  </text>
-                </g>
-              )
-            })
-          ) : (
-            'printerDatas is empty'
-          )
+          printerDatas.map((printer) => {
+            const { x, y } = getCoordinates(printer.tplinkStampante.id);
+            return (
+              <g key={printer.tplinkStampante.id} style={{ ...printerStyle, cursor: 'pointer' }}>
+                <SvgIcon component={PrintIcon} x={x} y={y} width="80px" onClick={() => openPrinterModal(printer.tplinkStampante.id)} />
+                <rect x={x + 50} y={y + 300} width="140px" height="40px" fill="rgba(167,156,156,0.53)" rx="5px" ry="5px" />
+                <text x={x + 60} y={y + 325} fill="black" fontSize="15px">
+                  <tspan>{`Power: ${printer.tplinkStampante.power.value !== undefined ? printer.tplinkStampante.power.value : ''} W`}</tspan>
+                </text>
+              </g>
+            )
+          })
+        ) : (
+          'printerDatas is empty'
+        )
         }
       </svg>
 
