e49586c2bf43c3361a15f4bab0362bf0141f1a05
giorgia barachetti <giorgiabarachetti@gmail.com>
2023-06-26 10:52:06 +0200

    fix: deleting spaces between worlds in file

--- src/utils/interfaces/Interfaces.tsx
+++ src/utils/interfaces/Interfaces.tsx
@@ -1,101 +1,101 @@
 export interface Lights {
   room?: string;
   state: {
-      id: number;
-      output?: boolean;
-      apower?: number;
-      voltage?: number;
-      current?: number;
-      aenergy?: {
-        total?: number;
-        by_minute?: [number, number, number];
-        minute_ts?: number;
-      };
-      temperature?: {
-        tC?: number;
-        tF?: number;
-      };
-    }  
+    id: number;
+    output?: boolean;
+    apower?: number;
+    voltage?: number;
+    current?: number;
+    aenergy?: {
+      total?: number;
+      by_minute?: [number, number, number];
+      minute_ts?: number;
+    };
+    temperature?: {
+      tC?: number;
+      tF?: number;
+    };
+  }
 }
 
 export interface Printer {
   tplinkStampante: {
-        id: number
-        power: {
-            value: number,
-            unit: "W"
-        },
-        voltage: {
-            value: number,
-            unit: "V"
-        },
-        current: {
-            value: number,
-            unit: "A"
-        },
-        total: {
-            value: number,
-            unit: "kWh"
-        }
+    id: number
+    power: {
+      value: number,
+      unit: "W"
+    },
+    voltage: {
+      value: number,
+      unit: "V"
+    },
+    current: {
+      value: number,
+      unit: "A"
+    },
+    total: {
+      value: number,
+      unit: "kWh"
     }
+  }
 }
 
 export interface Energy {
   //id setted by me, inexistent in api
-    id : number,
-    powerUsed: number,
-    averagePowerUsed: number,
-    energyUsedF1: number,
-    energyUsedF2: number,
-    energyUsedF3: number,
-    currentHour : number
+  id: number,
+  powerUsed: number,
+  averagePowerUsed: number,
+  energyUsedF1: number,
+  energyUsedF2: number,
+  energyUsedF3: number,
+  currentHour: number
 }
 
-export interface PrinterStatus{
+export interface PrinterStatus {
   stato_presa: boolean
 }
 export interface Coffee {
   coffes: {
-      id: number,
-      accensioni: number,
-      coffeeCount: number
+    id: number,
+    accensioni: number,
+    coffeeCount: number
   },
   data: {
-      macchinettaCaffe: {
-          receivedData : {
-            volts: number;
-            ampere: number;
-            watt: number;
-          };
-          timestamp: number
-      },
-      totalCoffeeToday: number,
-      count1: number,
-      count2: number
+    macchinettaCaffe: {
+      receivedData: {
+        volts: number;
+        ampere: number;
+        watt: number;
+      };
+      timestamp: number
+    },
+    totalCoffeeToday: number,
+    count1: number,
+    count2: number
   }
 }
 
 export interface Niveus {
-    id: number,
-    data: {
-      receivedData: {
-        volts: number,
-        ampere: number,
-        watt: number
-      },
-      timestamp: string
-    }
+  id: number,
+  data: {
+    receivedData?: {
+      volts: number,
+      ampere: number,
+      watt: number
+    },
+    timestamp: string
+  }
 }
 
 export interface ChartData {
   power: {
-      timestamp: string;
-      power: number
+    timestamp: string;
+    power: number
   }[];
 }
 export interface ChartDataNiveus {
-      data:{
-        timestamp: string,
-        watt: number
-      }[];
+  data: {
+    timestamp: string,
+    watt: number
+  }[];
 }
