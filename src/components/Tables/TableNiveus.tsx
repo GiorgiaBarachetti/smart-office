ab48828220ae03aef92fdc129634c9be79cb5fcb
giorgia barachetti <giorgiabarachetti@gmail.com>
2023-06-26 10:49:29 +0200

    feat: adding datas control

--- src/components/Tables/TableNiveus.tsx
+++ src/components/Tables/TableNiveus.tsx
@@ -31,9 +31,9 @@ const TableNiveus = ({ loading, niveus }: Props) => {
 
       {niveus?.map((n) => (
       <TableRow key={n.id}>
-        <TableCell>{n.data.receivedData.watt} W</TableCell>
-        <TableCell>{n.data.receivedData.volts} V</TableCell>
-        <TableCell>{n.data.receivedData.ampere} A</TableCell>
+        <TableCell>{n?.data?.receivedData?.watt != undefined ? n.data.receivedData.watt : ''} W</TableCell>
+        <TableCell>{n?.data?.receivedData?.volts != undefined ? n.data.receivedData.volts : ''} V</TableCell>
+        <TableCell>{n?.data?.receivedData?.ampere != undefined ? n.data.receivedData.ampere : ''} A</TableCell>
       </TableRow>
       ))}
 
