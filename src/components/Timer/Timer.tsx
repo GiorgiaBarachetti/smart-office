a843a2c5c30cbcccc3171150de9d764ecd16690a
giorgia barachetti <giorgiabarachetti@gmail.com>
2023-07-03 12:32:57 +0200

    fix: return to 24h format

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,35 +1,36 @@
 import { Box, Typography } from '@mui/material'
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Timer = () => {
 
-const [date, setDate] = useState(new Date())
+  const [date, setDate] = useState(new Date())
 
-useEffect (()=> {
+  useEffect(() => {
 
-    const timer = setInterval(()=>setDate(new Date()), 1000)
-    return function cleanup(){
-        clearInterval(timer)
+    const timer = setInterval(() => setDate(new Date()), 1000)
+    return function cleanup() {
+      clearInterval(timer)
     }
-},[])
-
-const dateTime = () => {
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let partOfDay = hours >= 12 ? 'pm' : 'am';
-  //convert 24 format into 12 
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  minutes = parseInt(minutes.toString().padStart(2, '0'));
-  let clock = hours + ':' + minutes + ' ' + partOfDay;
- return clock;
- }
- 
+  }, [])
+/*
+  const dateTime = () => {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let partOfDay = hours >= 12 ? 'pm' : 'am';
+    //convert 24 format into 12 
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = parseInt(minutes < 4 ? '0' + minutes : minutes.toString()); ///parseInt((minutes.toString()).padStart(2, '0'));
+    let clock = hours + ':' + minutes + ' ' + partOfDay;
+    return clock;
+  }
+*/
 
   return (
-    <Box component='div' sx={{position:'absolute', right: '10px', textAlign:'end'}}>
-              <Typography>{date.toLocaleDateString('en-US')}</Typography>
-              <Typography>{dateTime()}</Typography>
+    <Box component='div' sx={{ position: 'absolute', right: '10px', textAlign: 'end' }}>
+      <Typography>{date.toLocaleDateString('eur')}</Typography>
+      {/*<Typography>{dateTime()}</Typography>*/}
+      <Typography>{date.getHours()}:{date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()}</Typography>
     </Box>
   )
 }
