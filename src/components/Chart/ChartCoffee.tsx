c0f3988207ea8bfc59828d02a4455f665a958735
giorgia barachetti <giorgiabarachetti@gmail.com>
2023-07-11 11:46:53 +0200

    feat: added a chart for kwh

--- src/components/Chart/ChartCoffee.tsx
+++ src/components/Chart/ChartCoffee.tsx
@@ -7,7 +7,7 @@ import SnackbarGeneral from "../Snackbar/SnackbarGeneral";
 
 interface ChartDataCoffee {
     data: {
-        timestamp: number,
+        timestamp: string,
         value: number,
     }[]
 }
@@ -15,51 +15,66 @@ interface ChartDataCoffee {
 const ChartCoffee = () => {
     const [message, setMessage] = useState('')
     const [open, setOpen] = useState(false);
+    const [CoffeeDatas, setCoffeeDatas] = useState<ChartDataCoffee[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [selectedDateRange, setSelectedDateRange] = useState('today');
+    
     const handleClose = () => {
         setOpen(false);
         setMessage('')
     };
-
-    const [CoffeeDatas, setCoffeeDatas] = useState<ChartDataCoffee[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-    let [selectedDateRange, setSelectedDateRange] = useState('today');
-
+    
     const handleDateRangeClick = (dateRange: string) => {
         setSelectedDateRange(dateRange);
         fetchCoffeeData(dateRange)
     }
 
     const fetchCoffeeData = async (range: string) => {
+        console.log('response')
+
         try {
             setIsLoading(true);
             const currentDate = new Date();
             let startDate = '';
-            let endDate = currentDate;
+            const endDate = currentDate;
+            console.log('2 response')
 
             switch (range) {
-                case 'today':
+                case 'today': {
+            console.log('222 response')
+
                     startDate = currentDate.toISOString().split('T')[0];
                     break;
-                case 'yesterday':
+                }
+                case 'yesterday': {
                     const yesterday = new Date(currentDate.getTime() - 48 * 60 * 60 * 1000);
                     startDate = yesterday.toISOString().split('T')[0];
                     break;
-                case 'lastWeek':
+                }
+                case 'lastWeek': {
                     const lastWeekStart = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000);
                     startDate = lastWeekStart.toISOString().split('T')[0];
                     break;
-                case 'lastMonth':
+                }
+                case 'lastMonth': {
                     const lastMonthStart = new Date(currentDate.getTime() - 30 * 24 * 60 * 60 * 1000);
                     startDate = lastMonthStart.toISOString().split('T')[0];
                     break;
+                }
                 default:
                     break;
             }
+            console.log('22fsd2 response')
+
             const response = await fetch(`${baseURL}${urlCoffee}/data/watt?start=${startDate}T00:00:00&end=${endDate}`);
+        console.log('3 response')
+
             const data = await response.json();
+            console.log(response)
             setCoffeeDatas(Array.isArray(data) ? data : [data]);
             setIsLoading(false);
-            if(response.ok){
+            console.log(data)
+            if (!response.ok) {
                 throw new Error
             }
         } catch (error) {
@@ -90,7 +105,20 @@ const ChartCoffee = () => {
         },
         vAxis: { title: "Watt", minValue: 0 },
         chartArea: { width: "50%", height: "60%" },
-    };
+        explorer: { 
+            actions: ['dragToZoom', 'rightClickToReset'],
+            axis: 'horizontal',
+            keepInBounds: true,
+            maxZoomIn: 4.0},
+          colors: ['#4f403d'],
+          legend: 'none',
+        };
+    
+    const chartData = CoffeeDatas[0]?.data || [];
+    const chartItems = chartData.map(({ timestamp, value }) => [
+        new Date(timestamp),
+        value
+    ]);
 
     return (
         <Box component='div' sx={{ padding: '20px', width: '90%', mx: 'auto' }}>
@@ -99,7 +127,7 @@ const ChartCoffee = () => {
                     <Typography variant='h6' textAlign={'center'}>Select a data range</Typography>
                     <Box component='div' sx={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
                         <Button sx={{ cursor: 'pointer', mx: '10px', ...SHADOWSTYLE }} variant={selectedDateRange === 'today' ? 'contained' : 'outlined'} onClick={() => handleDateRangeClick('today')}>
-                            Today
+                            TODAY
                         </Button>
                         <Button sx={{ cursor: 'pointer', mx: '10px', ...SHADOWSTYLE }} variant={selectedDateRange === 'yesterday' ? 'contained' : 'outlined'} onClick={() => handleDateRangeClick('yesterday')}>
                             LAST 48 HOURS
@@ -122,17 +150,11 @@ const ChartCoffee = () => {
                             chartType="LineChart"
                             data={[
                                 ['timestamp', 'value'],
-                                [
+                                /*[
                                     new Date(new Date().setHours(0, 0, 0, 0)),
-                                    0
-                                ],
-                                ...(CoffeeDatas[0]?.data || []).map(({ timestamp, value }) => {
-                                    return ([new Date(timestamp), value]);
-                                }),
-                                [
-                                    new Date(new Date()),
-                                    0
-                                ]
+                                    null
+                                ],*/
+                                ...chartItems,
                             ]}
                             options={options}
                         />
